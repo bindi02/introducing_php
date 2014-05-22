808df708f1c5a7a88f91370e3dd30510a106ac99
Bindiya Rana <bindiya02rana@gmail.com>
2014-05-22 11:29:27 -0700

    comments type in php

---
+++ comments.php
@@ -0,0 +1,13 @@
+<?php
+//comment type in php
+//php hve 3 type comment like 2 single line & mulitiline
+//tht consider as single line
+# tht also consider as single line
+/*
+tht consider as mulitiline 
+*/
+$name = ' 
+Bindi'; // tht consider as whitespace php ignore tht 
+$lastname= 'Rana';
+echo $name,'  ',$lastname;//tht add space o/p like Bindi Rana
+?>
