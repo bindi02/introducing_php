ae71e959c1504645d186c9e0f425467c5caf6b5e
Bindiya Rana <bindiya02rana@gmail.com>
2014-05-24 19:06:54 -0700

    string and joining string program

---
+++ joining_string1.php
@@ -0,0 +1,13 @@
+<?php
+$fname = 'bindi';
+$lname = 'rana';
+$book = "php's guide";
+$ebook = '"php\'s guide"';
+/*echo $book;
+echo '<br>';
+echo $ebook;
+echo '<br>';
+echo "I love $book";  */
+$fullname = $fname . ' ' .$lname;
+echo $fullname;
+?>
---
+++ string.php
@@ -0,0 +1,9 @@
+<?php
+$book = "php's guide";
+$ebook = '"php\'s guide"';
+echo $book;
+echo '<br>';
+echo $ebook;
+echo '<br>';
+echo "I love $book";
+?>
